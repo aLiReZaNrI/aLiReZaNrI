49a3816719bdb434d91b0f5de2a7583f9ffb83c1
aLiReZaNrI <30468645+aLiReZaNrI@users.noreply.github.com>
2017-07-26 17:40:16 +0430

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,43 @@
+do local _ = {
+  admins = {
+    {
+      102122598,
+      "@Hushmandpelast"
+    },
+    {
+      79859533,
+      "@Sadeghyar"
+    },
+    {
+      344999829,
+      "\\*-\\*"
+    },
+    {
+      349172389,
+      "@Baabakii"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "BanHammer",
+    "Fun",
+    "GroupManager",
+    "Msg-Checks",
+    "Plugins",
+    "Tools",
+    "Write"
+  },
+  info_text = "⚜ *MaTaDoR BoT v5.4*\n\n🌐 https://github.com/MrMahDiRoO/MaTaDoR\n\n🔖 *Admins :*\n》@MahDiRoO ➣ _Founder & Developer《_\n\n》@ToOfan《\n\n📜 *Our channel :*\n》@MaTaDoRTeaM《",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    364464502,
+    152158957,
+    311318781,
+    367838246,
+    389556467
+  }
+}
+return _
+end
